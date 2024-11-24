8135364a893580c6b702fdcd35177463eafd53ba
urmat4002 <urmat4002@gmail.com>
2024-11-24 16:25:46 +0600

    deploy vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/shlagbaum_frontend/',
   server: {
     host: '0.0.0.0',
   },
