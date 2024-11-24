3bde7ff5e4ed2c5e3b038126bf0cf3fbeeb223cd
urmat4002 <urmat4002@gmail.com>
2024-11-24 14:43:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react'
+import {Body} from './body'
+
+const App = () => {
+  const [socket, setSocket] = useState(null)
+  useEffect(() => {
+    const newSocket = new WebSocket(
+      // 'ws://192.168.1.104:8000',
+      'ws://afternoon-island-43499-10cea7ad17e2.herokuapp.com/',
+    )
+    newSocket.onopen = () => {
+      console.log('Connected to WebSocket')
+    }
+    setSocket(newSocket)
+  }, [])
+
+  return <div>{socket && <Body newSocket={socket} />}</div>
+}
+
+export default App
